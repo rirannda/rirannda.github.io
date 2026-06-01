177c1a707c185dbf1edee7445769dbbce6ed7b41
rirannda <167262429+rirannda@users.noreply.github.com>
2026-06-01 15:10:30 +0900

    Fix margin class in Header.svelte navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -81,7 +81,7 @@
 		></span>
 	</a>
 
-	<nav class="mr-5 gap-6 md:flex ml-auto hidden items-center" aria-label="Global navigation">
+	<nav class="mx-5 gap-6 md:flex ml-auto hidden items-center" aria-label="Global navigation">
 		{#each links as link (link.id)}
 			<a
 				href={resolve(`/#${link.id}`)}
